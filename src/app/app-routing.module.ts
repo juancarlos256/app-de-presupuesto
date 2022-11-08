b3383bbb1abc0cf1632d8654080fd2e03dbcbfa0
juan carlos <juancarlosmoreno356@gmail.com>
2022-11-07 21:06:07 -0500

    se agrgaeron unos estilos y lo mas basicos para que se ejecute la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// componentes
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { GastosComponent } from './components/gastos/gastos.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/ingrearPresupuesto', pathMatch: 'full' },
+  { path: 'ingrearPresupuesto', component: IngresarPresupuestoComponent },
+  { path: 'gastos', component: GastosComponent },
+  { path: '**', redirectTo: '/ingrearPresupuesto', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
